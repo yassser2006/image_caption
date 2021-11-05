93a673221b06e0bc1c921b306a948139d1c23e73
Yasser Ibrahim <yassser2006>
2021-11-05 20:20:55 +0200

    First Commit

---
+++ model.py
@@ -0,0 +1,48 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet50(pretrained=True)
+        for param in resnet.parameters():
+            param.requires_grad_(False)
+        
+        modules = list(resnet.children())[:-1]
+        self.resnet = nn.Sequential(*modules)
+        self.embed = nn.Linear(resnet.fc.in_features, embed_size)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = features.view(features.size(0), -1)
+        features = self.embed(features)
+        return features
+    
+
+class DecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+    
+    def forward(self, features, captions):
+        embed = self.embed(captions[:,:-1])
+        embed = torch.cat((features.unsqueeze(1), embed), 1)
+        hidden, _ = self.lstm(embed)
+        output = self.linear(hidden)
+        return output
+
+    def sample(self, inputs, states=None, max_len=20):
+        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+        ids = []
+        for i in range(max_len):
+            hidden, states = self.lstm(inputs, states)          
+            output = self.linear(hidden)            
+            _, predicted = output.max(2)                        
+            ids.append(predicted.item())
+            inputs = self.embed(predicted)                       
+            
+        return ids
